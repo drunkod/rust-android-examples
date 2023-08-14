ffe6d360b6742145946f37b7c3b72b801a378fb0
Alex Acer <drunkod@exampl.com>
2023-08-14 18:12:39 +0500

    add ui window with button

--- agdk-eframe/src/lib.rs
+++ agdk-eframe/src/lib.rs
@@ -1,18 +1,97 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
 use eframe::egui;
-use eframe::{NativeOptions, Renderer};
+use eframe::NativeOptions;
+use eframe::Renderer;
+extern crate gstreamer as gst;
+//extern crate gstreamer_app as gst_app;
+//extern crate gstreamer_gl_sys as gst_gl_sys;
+
+use gst::prelude::*;
+//use gst::init;
 
 #[cfg(target_os = "android")]
 use winit::platform::android::activity::AndroidApp;
 
 #[derive(Default)]
-struct DemoApp {
-    demo_windows: egui_demo_lib::DemoWindows,
+struct MyApp {
+    name: String,
+    label: String,
+    age: u32,
+    value: u32,
 }
 
-impl eframe::App for DemoApp {
+impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        self.demo_windows.ui(ctx);
+     egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            // The top panel is often a good place for a menu bar:
+            egui::menu::bar(ui, |ui| {
+                ui.menu_button("File", |ui| {
+                    if ui.button("Quit").clicked() {
+                        _frame.close();
+                    }
+                });
+            });
+        });
+
+        egui::SidePanel::left("side_panel").show(ctx, |ui| {
+            ui.heading("Side Panel");
+
+            ui.horizontal(|ui| {
+                ui.label("Write something: ");
+
+            });
+
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+                    let button = ui.button("Click me");
+                    if button.clicked() {
+                        // Call a function when the button is clicked
+                        self.age += 1;
+                          do_something();
+                    }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+
+                      // fn do_something() {
+                          // Initialize GStreamer
+                       //  gst::init().unwrap();
+
+                        // Do something when the button is clicked.
+                        //   println!("The button was clicked!");
+                       //  }
+
+
+
+            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                ui.horizontal(|ui| {
+                    ui.spacing_mut().item_spacing.x = 0.0;
+                    ui.label("powered by ");
+                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+                    ui.label(" and ");
+                    ui.hyperlink_to(
+                        "eframe",
+                        "https://github.com/emilk/egui/tree/master/crates/eframe",
+                    );
+                    ui.label(".");
+                });
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+
+        });
     }
+}
+fn do_something() {
+
+    // Initialize GStreamer
+
+    gst::init().unwrap();
 }
 
 fn _main(mut options: NativeOptions) -> eframe::Result<()> {
@@ -20,7 +99,7 @@ fn _main(mut options: NativeOptions) -> eframe::Result<()> {
     eframe::run_native(
         "My egui App",
         options,
-        Box::new(|_cc| Box::<DemoApp>::default()),
+        Box::new(|_cc| Box::<MyApp>::default()),
     )
 }
 
