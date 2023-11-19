9703a5bb7231c8357205d8135698a33195c901ad
drunkod <drunkod@gmail.com>
2023-11-19 15:01:25 -0500

    Update Android.mk

--- gstreamer-android/jni/Android.mk
+++ gstreamer-android/jni/Android.mk
@@ -12,9 +12,9 @@ $(error GSTREAMER_ROOT_ANDROID is not defined!)
 endif
 
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/armeabi-v7a
+GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/armv7
 else ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
-GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/arm64-v8a
+GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/arm64
 else ifeq ($(TARGET_ARCH_ABI),x86)
 GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/x86
 else ifeq ($(TARGET_ARCH_ABI),x86_64)
